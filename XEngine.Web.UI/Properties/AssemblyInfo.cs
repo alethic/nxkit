719e67c910b5f5a199bb7c2405ac62a1f53cf26b
Jerome Haltom <wasabi@larvalstage.net>
2013-03-16 01:31:13 -0500

    More rename.

--- XEngine.Web.UI/Properties/AssemblyInfo.cs
+++ XEngine.Web.UI/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("XEngine.Web.UI")]
+[assembly: AssemblyTitle("XEngine.Forms.Web.UI")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("XEngine.Web.UI")]
+[assembly: AssemblyProduct("XEngine.Forms.Web.UI")]
 [assembly: AssemblyCopyright("Copyright © Jerome Haltom 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
