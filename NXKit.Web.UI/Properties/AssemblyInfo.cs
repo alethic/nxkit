dcc244a34e5a1a728691811d3e79f31cbad12d28
Jerome Haltom <wasabi@larvalstage.net>
2013-03-18 11:23:21 -0500

    Renamed directory.

--- NXKit.Web.UI/Properties/AssemblyInfo.cs
+++ NXKit.Web.UI/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NXKit.Web.UI")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright © Jerome Haltom 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
