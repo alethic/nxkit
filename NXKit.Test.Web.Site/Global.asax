22ab0d8e75a5db6d6ce191f6e8161dfe84c0b974
Jerome Haltom <wasabi@larvalstage.net>
2013-03-18 11:36:45 -0500

    * More nuspec files.

---
+++ NXKit.Test.Web.Site/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NXKit.Test.Web.Site.Global" Language="C#" %>
---
+++ NXKit.Test.Web.Site/Global.asax.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Web.UI;
+
+namespace NXKit.Test.Web.Site
+{
+
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            ScriptManager.ScriptResourceMapping.AddDefinition("jquery", new ScriptResourceDefinition
+            {
+                Path = "~/Scripts/jquery-1.6.4.min.js",
+                DebugPath = "~/Scripts/jquery-1.6.4.js",
+                CdnPath = "http://ajax.microsoft.com/ajax/jQuery/jquery-1.6.4.min.js",
+                CdnDebugPath = "http://ajax.microsoft.com/ajax/jQuery/jquery-1.6.4.js"
+            });
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+
+}
