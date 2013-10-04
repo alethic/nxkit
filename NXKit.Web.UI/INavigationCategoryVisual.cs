a862ffcf5bf58b64fb8d66f86fc2ad3f275dc1af
Jerome Haltom <jhaltom@isillc.com>
2013-10-04 14:49:59 -0500

    These files were in the wrong place.

---
+++ NXKit.Web.UI/INavigationCategoryVisual.cs
@@ -0,0 +1,14 @@
+﻿namespace NXKit.Web.UI
+{
+
+    /// <summary>
+    /// Marks a visual as being navigatable.
+    /// </summary>
+    public interface INavigationCategoryVisual : INavigationVisual
+    {
+
+
+
+    }
+
+}
