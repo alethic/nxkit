7559cf89a0fb1946838079ad4c9f151cd4ae281c
Jerome Haltom <jhaltom@isillc.com>
2014-02-13 18:41:16 -0600

    Left off some files.

---
+++ NXKit.Tests/EngineTests.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.Xml.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NXKit.Tests
+{
+
+    [TestClass]
+    public class EngineTests
+    {
+
+        /// <summary>
+        /// Opens a new instance of the sample XML.
+        /// </summary>
+        /// <returns></returns>
+        Stream OpenSampleXml()
+        {
+            return typeof(EngineTests).Assembly.GetManifestResourceStream("NXKit.Tests.SampleXml.xml");
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var engine = new Engine(XDocument.Load(OpenSampleXml()), null);
+        }
+
+    }
+
+}
---
+++ NXKit.Wpf.UI/View.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace NXKit.Wpf.UI
+{
+
+    public class View :
+        Control
+    {
+
+        static View()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(View), new FrameworkPropertyMetadata(typeof(View)));
+        }
+
+    }
+
+}
