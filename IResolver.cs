47a248c37573183995b62e2a5ad0993059a2eac7
Jerome Haltom <wasabi@cogito.cx>
2014-03-07 23:42:30 -0600

    Beginnings of WPF support. View which supports a source. Renamed IResourceResolver to IResolver. Not just for resources. Going to rebase Engine loading support to file names that can be resolved.

---
+++ IResolver.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+
+namespace NXKit
+{
+
+    /// <summary>
+    /// Provides an interface for the forms processor to obtain and save resources.
+    /// </summary>
+    public interface IResolver
+    {
+
+        Stream Get(string href, string baseUri);
+
+        Stream Put(string href, string baseUri, Stream stream);
+
+    }
+
+}
