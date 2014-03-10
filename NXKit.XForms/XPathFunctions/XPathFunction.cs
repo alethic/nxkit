03370c4711c88af3629b000df054ba7dcc5945df
Jerome Haltom <jhaltom@isillc.com>
2014-03-10 14:16:59 -0500

    Methods to determine stuff from scope.

--- NXKit.XForms/XPathFunctions/XPathFunction.cs
+++ NXKit.XForms/XPathFunctions/XPathFunction.cs
@@ -1,10 +1,17 @@
-﻿using System.Xml.XPath;
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Xml.Linq;
+using System.Xml.XPath;
 using System.Xml.Xsl;
 
 namespace NXKit.XForms.XPathFunctions
 {
 
-    internal abstract class XPathFunction : IXsltContextFunction
+    /// <summary>
+    /// Base XPath function for XForms.
+    /// </summary>
+    internal abstract class XPathFunction :
+        IXsltContextFunction
     {
 
         public abstract int Maxargs { get; }
@@ -32,6 +39,69 @@ namespace NXKit.XForms.XPathFunctions
         object IXsltContextFunction.Invoke(XsltContext xsltContext, object[] args, XPathNavigator docContext)
         {
             return Invoke((VisualXmlNamespaceContext)xsltContext, docContext, args);
+        }
+
+        /// <summary>
+        /// Gets the current model of the navigator.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="navigator"></param>
+        /// <returns></returns>
+        protected XFormsModelVisual GetModel(VisualXmlNamespaceContext context, XPathNavigator navigator)
+        {
+            Contract.Requires<ArgumentNullException>(context != null);
+            Contract.Requires<ArgumentNullException>(navigator != null);
+            Contract.Requires<ArgumentNullException>(navigator.UnderlyingObject != null);
+            Contract.Requires<ArgumentNullException>(navigator.UnderlyingObject is XObject);
+
+            return GetModel(context.Visual.Engine.GetModule<XFormsModule>(), navigator);
+        }
+
+        /// <summary>
+        /// Gets the current instance of the navigator.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="navigator"></param>
+        /// <returns></returns>
+        protected XFormsInstanceVisual GetInstance(VisualXmlNamespaceContext context, XPathNavigator navigator)
+        {
+            Contract.Requires<ArgumentNullException>(context != null);
+            Contract.Requires<ArgumentNullException>(navigator != null);
+            Contract.Requires<ArgumentNullException>(navigator.UnderlyingObject != null);
+            Contract.Requires<ArgumentNullException>(navigator.UnderlyingObject is XObject);
+
+            return GetInstance(context.Visual.Engine.GetModule<XFormsModule>(), navigator);
+        }
+
+        /// <summary>
+        /// Gets the current model of the navigator.
+        /// </summary>
+        /// <param name="module"></param>
+        /// <param name="navigator"></param>
+        /// <returns></returns>
+        XFormsModelVisual GetModel(XFormsModule module, XPathNavigator navigator)
+        {
+            Contract.Requires<ArgumentNullException>(module != null);
+            Contract.Requires<ArgumentNullException>(navigator != null);
+            Contract.Requires<ArgumentNullException>(navigator.UnderlyingObject != null);
+            Contract.Requires<ArgumentNullException>(navigator.UnderlyingObject is XObject);
+
+            return module.GetModelItemModel((XObject)navigator.UnderlyingObject);
+        }
+
+        /// <summary>
+        /// Gets the current instance of the navigator.
+        /// </summary>
+        /// <param name="module"></param>
+        /// <param name="navigator"></param>
+        /// <returns></returns>
+        XFormsInstanceVisual GetInstance(XFormsModule module, XPathNavigator navigator)
+        {
+            Contract.Requires<ArgumentNullException>(navigator != null);
+            Contract.Requires<ArgumentNullException>(navigator.UnderlyingObject != null);
+            Contract.Requires<ArgumentNullException>(navigator.UnderlyingObject is XObject);
+
+            return module.GetModelItemInstance((XObject)navigator.UnderlyingObject);
         }
 
     }
