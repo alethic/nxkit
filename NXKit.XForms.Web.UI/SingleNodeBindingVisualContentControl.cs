dba6e7b51cfe9676d9e6276b5bbd19af6209df04
Jerome Haltom <jhaltom@isillc.com>
2014-03-12 14:13:58 -0500

    Base control type for visuals that are single node bound. Will provide methods for when relevant propoerties change, or events are raised, such as enabled/disabled.

---
+++ NXKit.XForms.Web.UI/SingleNodeBindingVisualContentControl.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+
+using NXKit.Web.UI;
+
+namespace NXKit.XForms.Web.UI
+{
+
+    /// <summary>
+    /// Abstract base for <see cref="VisualControl"/> instances that associated with <see cref="XFormsSingleNodeBindingVisual"/>s.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public abstract class SingleNodeBindingVisualContentControl<T> :
+        VisualContentControl<T>
+        where T : XFormsSingleNodeBindingVisual
+    {
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="view"></param>
+        /// <param name="visual"></param>
+        protected SingleNodeBindingVisualContentControl(View view, T visual)
+            : base(view, visual)
+        {
+            Contract.Requires<ArgumentNullException>(view != null);
+            Contract.Requires<ArgumentNullException>(visual != null);
+
+            visual.AddEventHandler<XFormsEnabledEvent>(Visual_Enabled, false);
+            visual.AddEventHandler<XFormsDisabledEvent>(Visual_Disabled, false);
+        }
+
+        void Visual_Enabled(Event evt)
+        {
+            OnVisualEnabled();
+        }
+
+        /// <summary>
+        /// Invoked when the underlying visual is enabled.
+        /// </summary>
+        protected virtual void OnVisualEnabled()
+        {
+
+        }
+
+        void Visual_Disabled(Event evt)
+        {
+            OnVisualDisabled();
+        }
+
+        /// <summary>
+        /// Invoked when the underlying visual is disabled.
+        /// </summary>
+        protected virtual void OnVisualDisabled()
+        {
+
+        }
+
+    }
+
+}
