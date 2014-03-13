9417ba16bbe154ef4bb8aaf21721138e5763d16d
Jerome Haltom <jhaltom@isillc.com>
2014-03-13 12:54:49 -0500

    Oops left these out.

---
+++ NXKit.XForms.Web.UI/InputEditableDefault.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+
+namespace NXKit.XForms.Web.UI
+{
+
+    [Priority(int.MinValue)]
+    public class InputEditableDefault :
+        InputEditableString
+    {
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="view"></param>
+        /// <param name="visual"></param>
+        public InputEditableDefault(NXKit.Web.UI.View view, XFormsInputVisual visual)
+            : base(view, visual)
+        {
+            Contract.Requires<ArgumentNullException>(view != null);
+            Contract.Requires<ArgumentNullException>(visual != null);
+        }
+
+    }
+
+}
---
+++ NXKit.XForms.Web.UI/RangeEditableDate.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+
+using Telerik.Web.UI;
+using Telerik.Web.UI.Calendar;
+
+using NXKit.Web.UI;
+
+namespace NXKit.XForms.Web.UI
+{
+
+    [XFormsXsdType(XmlSchemaConstants.XMLSchema_NS, "date")]
+    public class RangeEditableDate :
+        RangeEditable
+    {
+
+        RadDatePicker ctl;
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="view"></param>
+        /// <param name="visual"></param>
+        public RangeEditableDate(View view, XFormsRangeVisual visual)
+            : base(view, visual)
+        {
+            Contract.Requires<ArgumentNullException>(view != null);
+            Contract.Requires<ArgumentNullException>(visual != null);
+        }
+
+        /// <summary>
+        /// Creates the child control hierarchy.
+        /// </summary>
+        protected override void CreateChildControls()
+        {
+            ctl = new RadDatePicker();
+            ctl.ID = "ctl";
+            ctl.EnableViewState = false;
+            ctl.Calendar.EnableMonthYearFastNavigation = true;
+            ctl.SelectedDateChanged += ctl_SelectedDateChanged;
+
+            ctl.MinDate = DateTime.MinValue;
+            ctl.MaxDate = DateTime.MaxValue;
+
+            DateTime startDate;
+            if (DateTime.TryParse(Visual.Start, out startDate))
+                ctl.MinDate = startDate;
+
+            DateTime endDate;
+            if (DateTime.TryParse(Visual.End, out endDate))
+                ctl.MaxDate = endDate;
+
+            Controls.Add(ctl);
+        }
+
+        void ctl_SelectedDateChanged(object sender, SelectedDateChangedEventArgs args)
+        {
+            BindingUtil.Set(Visual.Binding, ctl.SelectedDate);
+        }
+
+        public override string TargetID
+        {
+            get { return ctl.ClientID; }
+        }
+
+        protected override void OnVisualValueChanged()
+        {
+            ctl.SelectedDate = BindingUtil.Get<DateTime?>(Visual.Binding);
+        }
+
+        protected override void OnVisualReadOnlyOrReadWrite()
+        {
+            ctl.Enabled = !Visual.ReadOnly;
+        }
+
+    }
+
+}
