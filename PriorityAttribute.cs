dccec6f96d6d799ad85d1c3307d1846c2bcdba1b
Jerome Haltom <jhaltom@isillc.com>
2014-03-13 12:54:00 -0500

    Restructured how control implementations are located. Added provider hierarchies. Removed label from control editable/view portions, and moved into host. Will eventually need to be able to turn this off for specific layouts.

---
+++ PriorityAttribute.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace NXKit
+{
+
+    /// <summary>
+    /// Marks a class as having a certain priority.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class)]
+    public class PriorityAttribute :
+        Attribute
+    {
+
+        /// <summary>
+        /// Gets the priority set on the given type.
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public static int GetPriority(Type type)
+        {
+            return type.GetCustomAttributes<PriorityAttribute>(false).Select(i => i.Priority).FirstOrDefault();
+        }
+
+        readonly int priority;
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="priority"></param>
+        public PriorityAttribute(int priority)
+        {
+            this.priority = priority;
+        }
+
+        public int Priority
+        {
+            get { return priority; }
+        }
+
+    }
+
+}
--- XmlSchemaConstants.cs
+++ XmlSchemaConstants.cs
@@ -9,17 +9,17 @@ namespace NXKit
     public static class XmlSchemaConstants
     {
 
-        public static readonly string XML_NS = "http://www.w3.org/XML/1998/namespace";
+        public const string XML_NS = "http://www.w3.org/XML/1998/namespace";
 
         public static readonly XNamespace XML = XML_NS;
 
-        public static readonly string XML_HREF = "http://www.w3.org/2001/xml.xsd";
+        public const string XML_HREF = "http://www.w3.org/2001/xml.xsd";
 
-        public static readonly string XMLSchema_NS = "http://www.w3.org/2001/XMLSchema";
+        public const string XMLSchema_NS = "http://www.w3.org/2001/XMLSchema";
 
         public static readonly XNamespace XMLSchema = XMLSchema_NS;
 
-        public static readonly string XMLSchema_HREF = "http://www.w3.org/2001/XMLSchema.xsd";
+        public const string XMLSchema_HREF = "http://www.w3.org/2001/XMLSchema.xsd";
 
     }
 
