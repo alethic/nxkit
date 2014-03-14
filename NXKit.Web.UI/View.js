06143fa7ef10dbd15d8e07170b07b0b83d92628e
Jerome Haltom <wasabi@cogito.cx>
2014-03-13 20:38:09 -0500

    Added client side state property.

--- NXKit.Web.UI/View.js
+++ NXKit.Web.UI/View.js
@@ -1,7 +1,6 @@
 ﻿Type.registerNamespace('NXKit.Web.UI');
 
-NXKit.Web.UI.ValueChangedEventArgs = function (source, modelItemId, newValue, newValueHashCode)
-{
+NXKit.Web.UI.ValueChangedEventArgs = function (source, modelItemId, newValue, newValueHashCode) {
     this._source = source;
     this._modelItemId = modelItemId;
     this._newValue = newValue;
@@ -11,23 +10,19 @@ NXKit.Web.UI.ValueChangedEventArgs = function (source, modelItemId, newValue, ne
 NXKit.Web.UI.ValueChangedEventArgs.prototype =
 {
 
-    get_source: function ()
-    {
+    get_source: function () {
         return this._source;
     },
 
-    get_modelItemId: function ()
-    {
+    get_modelItemId: function () {
         return this._modelItemId;
     },
 
-    get_newValue: function () 
-    {
+    get_newValue: function () {
         return this._newValue;
     },
 
-    get_newValueHashCode: function () 
-    {
+    get_newValueHashCode: function () {
         return this._newValueHashCode;
     }
 
@@ -36,44 +31,47 @@ NXKit.Web.UI.ValueChangedEventArgs.prototype =
 NXKit.Web.UI.ValueChangedEventArgs.registerClass('NXKit.Web.UI.ValueChangedEventArgs', Sys.EventArgs);
 
 
-NXKit.Web.UI.View = function (element)
-{
-    NXKit.Web.UI.View.initializeBase(this, [element]);
+NXKit.Web.UI.View = function (element, state) {
+    NXKit.Web.UI.View.initializeBase(this, element);
+    self._state = state;
 };
 
 NXKit.Web.UI.View.prototype =
 {
 
-    initialize: function ()
-    {
+    initialize: function () {
         NXKit.Web.UI.View.callBaseMethod(this, 'initialize');
     },
 
-    dispose: function ()
-    {
+    dispose: function () {
         NXKit.Web.UI.View.callBaseMethod(this, 'dispose');
     },
 
-    add_valueChanged: function (handler)
-    {
+    add_valueChanged: function (handler) {
         this.get_events().addHandler('valueChanged', handler);
     },
 
-    remove_valueChanged: function (handler)
-    {
+    remove_valueChanged: function (handler) {
         this.get_events().removeHandler('valueChanged', handler);
     },
 
-    raiseValueChanged: function (args)
-    {
+    raiseValueChanged: function (args) {
         var h = this.get_events().getHandler('valueChanged');
         if (h)
             h(this, args);
+    },
+
+    get_state: function () {
+        return this._state;
+    },
+
+    set_state: function (value) {
+        this._state = value;
     },
 
 };
 
 NXKit.Web.UI.View.registerClass('NXKit.Web.UI.View', Sys.UI.Control);
 
-if (typeof(Sys) !== 'undefined')
+if (typeof (Sys) !== 'undefined')
     Sys.Application.notifyScriptLoaded();
