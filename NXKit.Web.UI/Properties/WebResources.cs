09b5958a581d16cfe4f383af300c0beb90b79129
Jerome Haltom <wasabi@cogito.cx>
2014-03-14 00:11:38 -0500

    Misssing files.

---
+++ NXKit.Web.UI/Properties/WebResources.cs
@@ -0,0 +1,6 @@
+﻿using System.Web.UI;
+
+[assembly: WebResource("NXKit.Web.UI.View.js", "text/javascript")]
+[assembly: WebResource("NXKit.Web.UI.TypeScript.View.js", "text/javascript")]
+[assembly: WebResource("NXKit.Web.UI.TypeScript.View.js.map", "text/javascript")]
+[assembly: WebResource("NXKit.Web.UI.RadValidationDecorator.js", "text/javascript")]
