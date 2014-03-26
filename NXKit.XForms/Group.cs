b39adec4d77b6e582a282c493c2a919137a36494
Jerome Haltom <jhaltom@isillc.com>
2014-03-26 11:13:59 -0500

    Group rename.

---
+++ NXKit.XForms/Group.cs
@@ -0,0 +1,38 @@
+﻿using System.Xml.Linq;
+
+namespace NXKit.XForms
+{
+
+    [Element("group")]
+    public class Group :
+        SingleNodeBindingElement,
+        ISupportsUiCommonAttributes,
+        IRelevancyScope
+    {
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        public Group()
+            : base()
+        {
+
+
+
+        }
+        
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="xml"></param>
+        public Group(XElement xml)
+            : base(xml)
+        {
+
+
+
+        }
+
+    }
+
+}
