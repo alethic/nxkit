520a60a906eb57125dfde1a281c2617bc0db654c
Jerome Haltom <jhaltom@isillc.com>
2014-04-03 16:27:49 -0500

    TypeScript stuff.

--- deploy.cmd
+++ deploy.cmd
@@ -75,9 +75,9 @@ IF /I "NXKit.sln" NEQ "" (
 
 :: 2. Build to the temporary path
 IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
-  call :ExecuteCmd "%MSBUILD_PATH%" "%DEPLOYMENT_SOURCE%\NXKit.Test.Web.Site\NXKit.Test.Web.Site.csproj" /nologo /verbosity:m /t:Rebuild /t:pipelinePreDeployCopyAllFilesToOneFolder /p:_PackageTempDir="%DEPLOYMENT_TEMP%";AutoParameterizationWebConfigConnectionStrings=false;Configuration=Release /p:SolutionDir="%DEPLOYMENT_SOURCE%\.\\" %SCM_BUILD_ARGS%
+  call :ExecuteCmd "%MSBUILD_PATH%" "%DEPLOYMENT_SOURCE%\NXKit.Test.Web.Site\NXKit.Test.Web.Site.csproj" /nologo /verbosity:m /t:Clean,Build /t:pipelinePreDeployCopyAllFilesToOneFolder /p:_PackageTempDir="%DEPLOYMENT_TEMP%";AutoParameterizationWebConfigConnectionStrings=false;Configuration=Release /p:SolutionDir="%DEPLOYMENT_SOURCE%\.\\" %SCM_BUILD_ARGS%
 ) ELSE (
-  call :ExecuteCmd "%MSBUILD_PATH%" "%DEPLOYMENT_SOURCE%\NXKit.Test.Web.Site\NXKit.Test.Web.Site.csproj" /nologo /verbosity:m /t:Rebuild /p:AutoParameterizationWebConfigConnectionStrings=false;Configuration=Release /p:SolutionDir="%DEPLOYMENT_SOURCE%\.\\" %SCM_BUILD_ARGS%
+  call :ExecuteCmd "%MSBUILD_PATH%" "%DEPLOYMENT_SOURCE%\NXKit.Test.Web.Site\NXKit.Test.Web.Site.csproj" /nologo /verbosity:m /t:Clean,Build /p:AutoParameterizationWebConfigConnectionStrings=false;Configuration=Release /p:SolutionDir="%DEPLOYMENT_SOURCE%\.\\" %SCM_BUILD_ARGS%
 )
 
 IF !ERRORLEVEL! NEQ 0 goto error
