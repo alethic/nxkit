69fab2153ff4b4351d3b48f4a1fc55796145bf36
Jerome Haltom <wasabi@cogito.cx>
2014-04-03 21:38:32 -0500

    Instance is no longer a special element.

---
+++ IInitialize.cs
@@ -0,0 +1,14 @@
+﻿namespace NXKit
+{
+
+    /// <summary>
+    /// Allows an interface to be initialized when an element is added.
+    /// </summary>
+    public interface IInitialize
+    {
+
+        void Init();
+
+    }
+
+}
