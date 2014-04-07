a1b9ea9e5c04240aadee8184399be57814955ca1
Jerome Haltom <jhaltom@isillc.com>
2014-04-07 17:00:18 -0500

    Fixed up

---
+++ NXKit.XForms/DeleteAttributes.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+
+namespace NXKit.XForms
+{
+
+    /// <summary>
+    /// Provides the XForms 'delete' attributes.
+    /// </summary>
+    public class DeleteAttributes :
+        ActionAttributes
+    {
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="element"></param>
+        public DeleteAttributes(NXElement element)
+            : base(element)
+        {
+            Contract.Requires<ArgumentNullException>(element != null);
+        }
+
+        /// <summary>
+        /// Gets the 'ref' attribute values.
+        /// </summary>
+        public string At
+        {
+            get { return GetAttributeValue("at"); }
+        }
+
+    }
+
+}
