d5f848dfba59d536ef05200ffa7883db3ea00bf1
Jerome Haltom <jhaltom@isillc.com>
2014-04-07 17:01:06 -0500

    Missed

---
+++ NXKit.XForms/Delete.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Xml.Linq;
+
+using NXKit.DOMEvents;
+
+namespace NXKit.XForms
+{
+
+    [NXElementInterface("{http://www.w3.org/2002/xforms}delete")]
+    public class Delete :
+        IAction
+    {
+
+        readonly NXElement element;
+        readonly DeleteAttributes attributes;
+        readonly Lazy<IBindingNode> bindingNode;
+        readonly Lazy<EvaluationContext> context;
+        readonly Lazy<Binding> atBinding;
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="element"></param>
+        public Delete(NXElement element)
+        {
+            Contract.Requires<ArgumentNullException>(element != null);
+
+            this.element = element;
+            this.attributes = new DeleteAttributes(element);
+            this.bindingNode = new Lazy<IBindingNode>(() => element.Interface<IBindingNode>());
+            this.context = new Lazy<EvaluationContext>(() => element.ResolveEvaluationContext());
+            this.atBinding = new Lazy<Binding>(() => attributes.At != null ? new Binding(element, context.Value, attributes.At) : null);
+        }
+
+        /// <summary>
+        /// Gets the XForms module.
+        /// </summary>
+        XFormsModule Module
+        {
+            get { return element.Document.Module<XFormsModule>(); }
+        }
+
+        /// <summary>
+        /// Gets the binding applied to the node.
+        /// </summary>
+        Binding Binding
+        {
+            get { return bindingNode.Value != null ? bindingNode.Value.Binding : null; }
+        }
+
+        public void Handle(Event ev)
+        {
+            Module.InvokeAction(this);
+        }
+
+        /// <summary>
+        /// Parses the return value of the 'at' expression.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        double ParseAt(string value)
+        {
+            // no value, signals all values to be deleted
+            if (value == null)
+                return double.PositiveInfinity;
+
+            double v;
+            if (double.TryParse(value, out v))
+                // round to nearest integer
+                return Math.Round(v);
+            else
+                // delete last node
+                return double.NaN;
+        }
+
+        public void Invoke()
+        {
+            // extract model items for processing
+            var items = Binding != null && Binding.ModelItems != null ? Binding.ModelItems.ToArray() : null;
+            if (items.Length == 0)
+                return;
+
+            // ensure we have at least one node
+            var firstNode = Binding.ModelItems.Cast<XElement>().FirstOrDefault();
+            if (firstNode == null)
+                return;
+
+            // must have a parent
+            var parent = firstNode.Parent;
+            if (parent == null)
+                throw new Exception();
+
+            // parent and children must be in the same namespace
+            if (parent.Name.Namespace != firstNode.Name.Namespace)
+                throw new Exception();
+
+            // start and end locations
+            var at = ParseAt(atBinding.Value != null ? atBinding.Value.Value : null);
+            int s, e;
+            if (at == double.PositiveInfinity)
+            {
+                s = 1;
+                e = items.Length;
+            }
+            else if (at == double.NaN)
+            {
+                s = items.Length;
+                e = items.Length;
+            }
+            else
+            {
+                s = (int)at;
+                e = (int)at;
+
+                // constrain to first element
+                if (s < 1)
+                    s = 1;
+
+                // constrain to last element
+                if (e > Binding.ModelItems.Length)
+                    e = Binding.ModelItems.Length;
+            }
+
+            // set of instances to dispatch delete events to
+            var dispatch = new HashSet<Instance>();
+
+            // cycle through nodes to delete in reverse
+            for (int i = e; i >= s; i--)
+            {
+                var item = items[i];
+                if (item == null)
+                    continue;
+
+                if (item.ReadOnly)
+                    continue;
+
+                var element = item.Xml as XElement;
+                if (element != null)
+                {
+                    if (element == item.Xml.Document.Root)
+                        continue;
+
+                    element.Remove();
+                }
+
+                var attribute = item.Xml as XAttribute;
+                if (attribute != null)
+                {
+                    if (attribute.IsNamespaceDeclaration)
+                        continue;
+
+                    attribute.Remove();
+                }
+
+                item.Model.State.RebuildFlag = true;
+                item.Model.State.RecalculateFlag = true;
+                item.Model.State.RevalidateFlag = true;
+                item.Model.State.RefreshFlag = true;
+
+                dispatch.Add(item.Instance);
+            }
+
+            // dispatch appropriate events
+            foreach (var instance in dispatch)
+                instance.Element.Interface<INXEventTarget>().DispatchEvent(Events.Delete);
+        }
+
+    }
+
+}
