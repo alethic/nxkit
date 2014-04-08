ef5d29d8f35cffb1dea7b6b8a3643f0fd07298f4
Jerome Haltom <jhaltom@isillc.com>
2014-04-08 17:22:45 -0500

    Introduced serialization classes to serialize annotations.

---
+++ IO/XAnnotationRootElement.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Xml.Linq;
+
+namespace NXKit.IO
+{
+
+    /// <summary>
+    /// Wraps an existing <see cref="XElement"/> modifying the body contents to include persisted annotation data.
+    /// </summary>
+    public class XAnnotationRootElement :
+        XAnnotationElement
+    {
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="element"></param>
+        public XAnnotationRootElement(XElement element)
+            : base(element)
+        {
+            Contract.Requires<ArgumentNullException>(element != null);
+        }
+
+        protected override IEnumerable<object> GetSaveContents()
+        {
+            foreach (var i in base.GetAnnotations(Source.Document))
+                yield return i;
+
+            foreach (var i in base.GetSaveContents())
+                yield return i;
+        }
+
+    }
+
+}
---
+++ IO/XDocumentAnnotationReader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Xml.Linq;
+
+namespace NXKit.IO
+{
+
+    /// <summary>
+    /// Provides a reader for a <see cref="XDocument"/> instance that attempts to provided serialized representations
+    /// of annotation data.
+    /// </summary>
+    public class XDocumentAnnotationReader :
+        XNodeReaderBase
+    {
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="document"></param>
+        public XDocumentAnnotationReader(XDocument document)
+            : base(new XAnnotationDocument(document))
+        {
+            Contract.Requires<ArgumentNullException>(document != null);
+        }
+
+        public new XDocument Source
+        {
+            get { return (XDocument)base.Source; }
+        }
+
+    }
+
+}
