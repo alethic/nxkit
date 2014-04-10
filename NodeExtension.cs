580a539b6bea645b48a3c6f59159dd65652ccef2
Jerome Haltom <jhaltom@isillc.com>
2014-04-10 16:05:12 -0500

    Stuff

---
+++ NodeExtension.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Xml.Linq;
+
+namespace NXKit
+{
+
+    /// <summary>
+    /// Base class for an extension on <see cref="XNode"/> instances.
+    /// </summary>
+    public abstract class NodeExtension
+    {
+
+        readonly XNode node;
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="node"></param>
+        public NodeExtension(XNode node)
+        {
+            Contract.Requires<ArgumentNullException>(node != null);
+
+            this.node = node;
+        }
+
+        /// <summary>
+        /// Gets the <see cref="XNode"/> this extension applies to.
+        /// </summary>
+        public XNode Node
+        {
+            get { return node; }
+        }
+
+    }
+
+}
