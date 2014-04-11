af89a1a6231024450dcb6a9e31bba702b3c50fd1
Jerome Haltom <wasabi@cogito.cx>
2014-04-11 03:27:06 -0500

    Implemented partial updates. Holy crap, it's fast. And it works. Lovely.
    Also doing JSON deserialiation differently.

---
+++ IOnLoad.cs
@@ -0,0 +1,17 @@
+﻿namespace NXKit
+{
+
+    /// <summary>
+    /// Invoked by the processor each time a document is loaded.
+    /// </summary>
+    public interface IOnLoad
+    {
+
+        /// <summary>
+        /// Invoked when the document has been loaded.
+        /// </summary>
+        void Load();
+
+    }
+
+}
