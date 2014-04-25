c78e4594c6114ef943e2be3f4150e8eb5b430768
Jerome Haltom <wasabi@cogito.cx>
2014-04-25 10:25:20 -0500

    Added some Composition stuff. Unused so far.

---
+++ NXKit/Composition/ScopeExportAttribute.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Diagnostics.Contracts;
+
+namespace NXKit.Composition
+{
+
+    /// <summary>
+    /// Marks a part as requiring a container supporting the specified scope.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = true)]
+    [MetadataAttribute]
+    public class ScopeExportAttribute :
+        ExportAttribute
+    {
+
+        readonly Scope scope;
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="contractName"></param>
+        /// <param name="contractType"></param>
+        /// <param name="scope"></param>
+        public ScopeExportAttribute(string contractName, Type contractType, Scope scope)
+            : base(contractName, contractType)
+        {
+            Contract.Requires<ArgumentException>(contractName != null || contractType != null);
+
+            this.scope = scope;
+        }
+
+        /// <summary>
+        /// Initializes a new instnace.
+        /// </summary>
+        /// <param name="contractType"></param>
+        /// <param name="scope"></param>
+        public ScopeExportAttribute(Type contractType, Scope scope)
+            : this(null, contractType, scope)
+        {
+            Contract.Requires<ArgumentNullException>(contractType != null);
+        }
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="contractType"></param>
+        public ScopeExportAttribute(Type contractType)
+            : this(contractType, Scope.Global)
+        {
+            Contract.Requires<ArgumentNullException>(contractType != null);
+        }
+
+        /// <summary>
+        /// Export is only available to the specified <see cref="scope"/>.
+        /// </summary>
+        public Scope Scope
+        {
+            get { return scope; }
+        }
+
+    }
+
+}
---
+++ NXKit/Composition/ScopeExportProvider.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using NXKit.Util;
+using System.ComponentModel.Composition.Hosting;
+using System.Diagnostics.Contracts;
+
+namespace NXKit.Composition
+{
+
+    /// <summary>
+    /// <see cref="ExportProvider"/> implementation that filters a parent <see cref="ExportProvider"/> for exports
+    /// either within the designated scope or outside of it.
+    /// </summary>
+    class ScopeExportProvider :
+        FilteredExportProvider
+    {
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="parent"></param>
+        /// <param name="scope"></param>
+        public ScopeExportProvider(ExportProvider parent, Scope scope = Scope.Global)
+            : base(parent, _ => ((Scope?)_.Metadata.GetOrDefault<string, object>("Scope") ?? Scope.Global) == scope)
+        {
+            Contract.Requires<ArgumentNullException>(parent != null);
+        }
+
+    }
+
+}
