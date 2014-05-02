9fedf687086df3eda7145db4c61a4ad4b996f14b
Jerome Haltom <jhaltom@isillc.com>
2014-05-02 10:59:30 -0500

    Fix test.

--- NXKit.Tests/DefaultInterfaceProviderTests.cs
+++ NXKit.Tests/DefaultInterfaceProviderTests.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Xml;
 using System.Xml.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NXKit.Composition;
 
 namespace NXKit.Tests
 {
@@ -27,6 +28,8 @@ namespace NXKit.Tests
         {
 
         }
+
+        static readonly HostContainer hostContainer = new HostContainer(CompositionUtil.CreateContainer(), CompositionUtil.DefaultCatalog);
 
         static readonly XNamespace NS = XNamespace.Get("http://tempuri.org");
 
@@ -66,7 +69,7 @@ namespace NXKit.Tests
         [TestMethod]
         public void Test_full_element()
         {
-            var p = new DefaultInterfaceProvider(CompositionUtil.CreateContainer(), predicates, descriptors);
+            var p = new DefaultInterfaceProvider(hostContainer, predicates, descriptors);
             var i = p.GetInterfaces(new XElement(NS + "element1")).ToArray();
             Assert.IsTrue(i.Length == 2);
             Assert.IsInstanceOfType(i[0], typeof(Object1));
@@ -75,7 +78,7 @@ namespace NXKit.Tests
         [TestMethod]
         public void Test_local_only_element()
         {
-            var p = new DefaultInterfaceProvider(CompositionUtil.CreateContainer(), predicates, descriptors);
+            var p = new DefaultInterfaceProvider(hostContainer, predicates, descriptors);
             var i = p.GetInterfaces(new XElement(NS + "element2")).ToArray();
             Assert.IsTrue(i.Length == 2);
             Assert.IsInstanceOfType(i[0], typeof(Object2));
@@ -84,7 +87,7 @@ namespace NXKit.Tests
         [TestMethod]
         public void Test_ns_only_element()
         {
-            var p = new DefaultInterfaceProvider(CompositionUtil.CreateContainer(), predicates, descriptors);
+            var p = new DefaultInterfaceProvider(hostContainer, predicates, descriptors);
             var i = p.GetInterfaces(new XElement(NS + "element3")).ToArray();
             Assert.IsTrue(i.Length == 1);
             Assert.IsInstanceOfType(i[0], typeof(Object3));
