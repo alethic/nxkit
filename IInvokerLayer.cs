d854c66c393696894dc716dcfd6d71e0a048bad0
Jerome Haltom <jhaltom@isillc.com>
2014-05-02 21:07:28 -0500

    Added an IInvokerLayer/IInvoker infrastructure. This allows code to trap entry into a call stack when dealing with documents. Retooled the ExceptionHandler stuff as an IInvokerLayer. Parts can now import IInvoker to ensure they are operating within a document context, so exceptions get bubbled properly. XVisitor/XTransformer were not entering into

---
+++ IInvokerLayer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace NXKit
+{
+
+    /// <summary>
+    /// Provides a context layer to an invocation within the document context.
+    /// </summary>
+    public interface IInvokerLayer
+    {
+
+        /// <summary>
+        /// Invokes the given <see cref="Action"/> within the document context.
+        /// </summary>
+        /// <param name="action"></param>
+        void Invoke(Action action);
+
+        /// <summary>
+        /// Invokes the given <see cref="Func`1"/> within the document context.
+        /// </summary>
+        /// <typeparam name="R"></typeparam>
+        /// <param name="func"></param>
+        /// <returns></returns>
+        R Invoke<R>(Func<R> func);
+
+    }
+
+}
