9132b122bc0b619cbc437c9229c412d1673a47a0
Jerome Haltom <jhaltom@isillc.com>
2014-05-15 18:47:22 -0500

    Introducing flex box stuff. All sorts of things. Stuff.

---
+++ DocumentEnvironment.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Diagnostics.Contracts;
+
+using NXKit.Composition;
+
+namespace NXKit
+{
+
+    /// <summary>
+    /// Provides access to document scope variables.
+    /// </summary>
+    [ScopeExport(typeof(DocumentEnvironment), Scope.Host)]
+    public class DocumentEnvironment
+    {
+
+        NXDocumentHost host;
+
+        /// <summary>
+        /// Gets the configured host.
+        /// </summary>
+        /// <returns></returns>
+        [ScopeExport(typeof(Func<NXDocumentHost>), Scope.Host)]
+        public NXDocumentHost GetHost()
+        {
+            return host;
+        }
+
+        internal void SetHost(NXDocumentHost host)
+        {
+            Contract.Requires<ArgumentNullException>(host != null);
+
+            this.host = host;
+        }
+
+    }
+
+}
