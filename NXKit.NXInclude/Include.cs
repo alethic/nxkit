a5a70c20238d4ed5ac9933b575d053573b9d6989
Jerome Haltom <jhaltom@isillc.com>
2014-07-10 16:57:58 -0500

    Missed file.

---
+++ NXKit.NXInclude/Include.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Xml.Linq;
+
+using NXKit.Diagnostics;
+using NXKit.IO;
+
+namespace NXKit.NXInclude
+{
+
+    [Interface("{http://schemas.nxkit.org/2014/NXInclude}include")]
+    public class Include :
+        NXKit.XInclude.Include
+    {
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="element"></param>
+        /// <param name="trace"></param>
+        /// <param name="io"></param>
+        /// <param name="properties"></param>
+        public Include(
+            XElement element,
+            Lazy<ITraceService> trace,
+            Lazy<IIOService> io,
+            Lazy<IncludeProperties> properties)
+            : base(element, trace, io, new Lazy<XInclude.IncludeProperties>(() => properties.Value))
+        {
+            Contract.Requires<ArgumentNullException>(element != null);
+            Contract.Requires<ArgumentNullException>(trace != null);
+            Contract.Requires<ArgumentNullException>(io != null);
+            Contract.Requires<ArgumentNullException>(properties != null);
+        }
+
+        public new IncludeProperties Properties
+        {
+            get { return (IncludeProperties)base.Properties; }
+        }
+
+        protected override void PostInsertXml(XElement xml)
+        {
+            xml.AddAnnotation(new IncludeScopeAnnotation());
+        }
+
+    }
+
+}
