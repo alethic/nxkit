6f143562068de846a614f3159b5298bdf6cb19a5
Jerome Haltom <jhaltom@isillc.com>
2014-07-10 15:43:20 -0500

    New NXInclude infrastructure. Inherits from existing XInclude infrastructure. Will support passing context values. XInclude infrastructure updated. Now overridable. Supports parsing text and XML contents. Sample include forms.

--- NXKit.XInclude/Include.cs
+++ NXKit.XInclude/Include.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics.Contracts;
 using System.Xml.Linq;
+
 using NXKit.Diagnostics;
 using NXKit.IO;
 using NXKit.Xml;
@@ -14,45 +15,132 @@ namespace NXKit.XInclude
         IOnInit
     {
 
-        readonly ITraceService trace;
-        readonly IIOService io;
-        readonly IncludeAttributes attributes;
+        readonly Lazy<ITraceService> trace;
+        readonly Lazy<IIOService> io;
+        readonly Lazy<IncludeProperties> properties;
 
         /// <summary>
         /// Initializes a new instance.
         /// </summary>
         /// <param name="element"></param>
-        public Include(XElement element, ITraceService trace, IIOService io)
+        /// <param name="trace"></param>
+        /// <param name="io"></param>
+        /// <param name="properties"></param>
+        public Include(
+            XElement element,
+            Lazy<ITraceService> trace,
+            Lazy<IIOService> io,
+            Lazy<IncludeProperties> properties)
             : base(element)
         {
             Contract.Requires<ArgumentNullException>(element != null);
             Contract.Requires<ArgumentNullException>(trace != null);
             Contract.Requires<ArgumentNullException>(io != null);
+            Contract.Requires<ArgumentNullException>(properties != null);
 
             this.trace = trace;
             this.io = io;
-            this.attributes = new IncludeAttributes(element);
+            this.properties = properties;
         }
 
-        public void Init()
+        public IncludeProperties Properties
         {
-            var uri = new Uri(attributes.Href ?? "", UriKind.RelativeOrAbsolute);
+            get { return properties.Value; }
+        }
+
+        protected ITraceService Trace
+        {
+            get { return trace.Value; }
+        }
+
+        protected IIOService IO
+        {
+            get { return io.Value; }
+        }
+
+        /// <summary>
+        /// Gets the absolute <see cref="Uri"/> to be used to retrieve the include.
+        /// </summary>
+        /// <returns></returns>
+        public Uri GetAbsoluteHref()
+        {
+            var uri = Properties.Href ?? new Uri("", UriKind.Relative);
             if (Element.GetBaseUri() != null && !uri.IsAbsoluteUri)
                 uri = new Uri(Element.GetBaseUri(), uri);
 
-            trace.Information("XInclude: {0}", uri);
+            return uri;
+        }
 
-            var xml = XDocument.Load(IOXmlReader.Create(io, uri));
+        /// <summary>
+        /// Invoked after the newly inserted XML has been added.
+        /// </summary>
+        /// <param name="xml"></param>
+        protected virtual void PostInsertXml(XElement xml)
+        {
+
+        }
+
+        /// <summary>
+        /// Includes the remote resource as an XML document.
+        /// </summary>
+        protected virtual void IncludeAsXml()
+        {
+            var uri = GetAbsoluteHref();
+            var xml = XDocument.Load(IOXmlReader.Create(IO, uri, Properties.Accept));
             if (xml != null)
             {
                 // annotate element and replace self in graph
                 var element = new XElement(xml.Root);
-                element.SetBaseUri(xml.BaseUri);
+                element.SetBaseUri(xml.BaseUri != "" ? xml.BaseUri : uri.ToString());
                 Element.AddBeforeSelf(element);
                 Element.Remove();
 
-                trace.Information("XInclude: {0} replaced node {1}", element, Element);
+                PostInsertXml(element);
             }
+        }
+
+        /// <summary>
+        /// Invoked afte rthe newly inserted text has been added.
+        /// </summary>
+        /// <param name="text"></param>
+        protected virtual void PostInsertText(XText text)
+        {
+
+        }
+
+        /// <summary>
+        /// Includes the remote resource as a text document.
+        /// </summary>
+        protected virtual void IncludeAsText()
+        {
+            var uri = GetAbsoluteHref();
+            var txt = IOTextReader.Create(IO, uri, Properties.Encoding).ReadToEnd();
+            if (txt != null)
+            {
+                var text = new XText(txt);
+                Element.AddBeforeSelf(txt);
+                Element.Remove();
+
+                PostInsertText(text);
+            }
+        }
+
+        protected virtual void ExecuteInclude()
+        {
+            switch (Properties.Parse)
+            {
+                case IncludeParse.Xml:
+                    IncludeAsXml();
+                    break;
+                case IncludeParse.Text:
+                    IncludeAsText();
+                    break;
+            }
+        }
+
+        public void Init()
+        {
+            ExecuteInclude();
         }
 
     }
