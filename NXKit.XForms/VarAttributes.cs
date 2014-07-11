e0c4e2ca5b2d82e0090cdb9c97f457c127a62761
Jerome Haltom <jhaltom@isillc.com>
2014-07-10 19:48:51 -0500

    Starting work on Var.

---
+++ NXKit.XForms/VarAttributes.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Xml.Linq;
+
+namespace NXKit.XForms
+{
+
+    /// <summary>
+    /// Provides the XForms 'var' element attributes.
+    /// </summary>
+    public class VarAttributes :
+        AttributeAccessor
+    {
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="element"></param>
+        public VarAttributes(XElement element)
+            : base(element)
+        {
+            Contract.Requires<ArgumentNullException>(element != null);
+        }
+
+        /// <summary>
+        /// Gets the 'name' attribute values.
+        /// </summary>
+        public string Name
+        {
+            get { return GetAttributeValue("name"); }
+        }
+
+        /// <summary>
+        /// Gets the 'value' attribute values.
+        /// </summary>
+        public string Value
+        {
+            get { return GetAttributeValue("value"); }
+        }
+    }
+
+}
---
+++ NXKit.XForms/Xml/XElementExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Xml.Linq;
+
+namespace NXKit.XForms.Xml
+{
+
+    public static class XElementExtensions
+    {
+
+        /// <summary>
+        /// Gets the value of a given variable from a given <see cref="XElement"/>.
+        /// </summary>
+        /// <param name="self"></param>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public object GetVariableValue(this XElement self, string name)
+        {
+            Contract.Requires<ArgumentNullException>(self != null);
+            Contract.Requires<ArgumentNullException>(name != null);
+
+            // search referencable objects for id, or obtain from cache
+            return GetVariables(self)
+                .Where(i => i.Name == name)
+                .Select(i => i.Value)
+                .FirstOrDefault();
+        }
+
+        /// <summary>
+        /// Returns an enumeration of canidate 'var' elements implementations.
+        /// </summary>
+        /// <param name="self"></param>
+        /// <returns></returns>
+        static IEnumerable<Var> GetVariables(this XElement self)
+        {
+            yield break;
+        }
+
+    }
+
+}
