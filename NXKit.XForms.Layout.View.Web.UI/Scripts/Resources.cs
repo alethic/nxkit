e9cbeb77a8fcde0bc48a0aacadb1311493e973b9
Jerome Haltom <jhaltom@isillc.com>
2014-09-25 17:59:31 -0500

    Missed.

---
+++ NXKit.XForms.Layout.View.Web.UI/Scripts/Resources.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.UI;
+
+[assembly: WebResource("NXKit.XForms.Layout.View.Web.UI.Scripts.nxkit-xforms-layout.js", "application/javascript")]
+
+[assembly: System.Web.PreApplicationStartMethod(typeof(NXKit.XForms.Layout.View.Web.UI.PreApplication), "Start")]
+
+namespace NXKit.XForms.Layout.View.Web.UI
+{
+
+    public static class PreApplication
+    {
+
+        public static void Start()
+        {
+            if (ScriptManager.ScriptResourceMapping.GetDefinition("nxkit-xforms-layout") == null)
+                ScriptManager.ScriptResourceMapping.AddDefinition("nxkit-xforms-layout", new ScriptResourceDefinition()
+                {
+                    ResourceAssembly = typeof(PreApplication).Assembly,
+                    ResourceName = "NXKit.XForms.Layout.View.Web.UI.Scripts.nxkit-xforms-layout.js",
+                    LoadSuccessExpression = "window.NXKit.XForms.Layout",
+                });
+        }
+
+    }
+
+}
---
+++ NXKit.XForms.View.Web.UI/Scripts/Resources.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.UI;
+
+[assembly: WebResource("NXKit.XForms.View.Web.UI.Scripts.nxkit-xforms.js", "application/javascript")]
+
+[assembly: System.Web.PreApplicationStartMethod(typeof(NXKit.XForms.View.Web.UI.PreApplication), "Start")]
+
+namespace NXKit.XForms.View.Web.UI
+{
+
+    public static class PreApplication
+    {
+
+        public static void Start()
+        {
+            if (ScriptManager.ScriptResourceMapping.GetDefinition("nxkit-xforms") == null)
+                ScriptManager.ScriptResourceMapping.AddDefinition("nxkit-xforms", new ScriptResourceDefinition()
+                {
+                    ResourceAssembly = typeof(PreApplication).Assembly,
+                    ResourceName = "NXKit.XForms.View.Web.UI.Scripts.nxkit-xforms.js",
+                    LoadSuccessExpression = "window.NXKit.XForms",
+                });
+        }
+
+    }
+
+}
