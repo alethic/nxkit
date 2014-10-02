a80ed8ee8a197a24a8f0b3c7813cdfb1d780cfab
Jerome Haltom <jhaltom@isillc.com>
2014-10-02 12:51:13 -0500

    Raise proper errors from nx-html so onError can catch and redirect.

--- NXKit.View.Js/nx-html.js
+++ NXKit.View.Js/nx-html.js
@@ -1,37 +1,40 @@
 ﻿NXKit.define(['require', 'jquery'], function (require, $) {
     return {
+
         load: function (name, parentRequire, onload, config) {
             // find or create host container
-            var div1 = $('body>*[data-nx-html-host]').get(0);
-            if (div1 == null)
-                div1 = $(document.createElement('div'))
+            var host = $('body>*[data-nx-html-host]').get(0);
+            if (host == null)
+                host = $(document.createElement('div'))
                     .attr('data-nx-html-host', '')
                     .css('display', 'none')
                     .prependTo('body')
                     .get(0);
 
             // find or begin load
-            var div2 = $(div1).children("[data-nx-require='" + name + "']").get(0);
-            if (div2 == null) {
+            var div = $(host).children("[data-nx-html='" + name + "']").get(0);
+            if (div == null) {
                 var url = require.toUrl(name);
                 if (url) {
-                    div2 = $(document.createElement('div'))
-                        .attr('data-nx-require', name)
+                    div = $(document.createElement('div'))
+                        .attr('data-nx-html', name)
                         .load(url, function (response, status) {
                             if (status === 'success' || status === 'notmodified') {
 
                                 // append new element to host container
-                                $(div2).appendTo($(div1));
+                                $(div).appendTo($(host));
 
                                 // notify of load
-                                onload(div2);
+                                onload(div);
                             } else {
                                 // notify of error
-                                onload.error(status);
+                                onload.error(require.makeError('nx-html', status, new Error(status), [name]));
                             }
                         })
                         .get(0);
                 }
+            } else {
+                onload(div);
             }
         },
     }
