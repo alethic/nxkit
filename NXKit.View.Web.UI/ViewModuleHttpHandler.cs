16416b129327e5042671450c26b849f146f35b2e
Jerome Haltom <jhaltom@isillc.com>
2014-10-06 11:00:13 -0500

    Switch logic into ViewModuleHttpHandler. Added basic caching support.

---
+++ NXKit.View.Web.UI/ViewModuleHttpHandler.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Web;
+
+using NXKit.Composition;
+
+namespace NXKit.View.Web.UI
+{
+
+    /// <summary>
+    /// Handles NXKit.axd requests.
+    /// </summary>
+    public class ViewModuleHttpHandler :
+        IHttpHandler
+    {
+
+        public bool IsReusable
+        {
+            get { return true; }
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            // allow browser to cache response
+            context.Response.Cache.SetCacheability(HttpCacheability.Public);
+            context.Response.Cache.SetMaxAge(new TimeSpan(1, 0, 0));
+            context.Response.StatusCode = 404;
+
+            // look up module by name
+            var name = context.Request.QueryString["m"];
+            var file = CompositionUtil.CreateContainer(CompositionUtil.DefaultGlobalCatalog)
+                .GetExportedValues<IViewModuleResolver>()
+                .Select(i => i.Resolve(name))
+                .FirstOrDefault(i => i != null);
+
+            if (file != null)
+            {
+                // send module data
+                context.Response.StatusCode = 200;
+                file(context.Response);
+            }
+        }
+
+    }
+
+}
--- NXKit.View.Web.UI/ViewModuleHttpModule.cs
+++ NXKit.View.Web.UI/ViewModuleHttpModule.cs
@@ -1,20 +1,23 @@
 ﻿using System;
-using System.Linq;
 using System.Web;
-
-using NXKit.Composition;
 
 [assembly: PreApplicationStartMethod(typeof(NXKit.View.Web.UI.ViewModuleHttpModule), "Start")]
 
 namespace NXKit.View.Web.UI
 {
 
+    /// <summary>
+    /// Maps NXKit.axd requests to the <see cref="ViewModuleHttpHandler"/>.
+    /// </summary>
     public class ViewModuleHttpModule :
         IHttpModule
     {
 
         static readonly string PREFIX = @"~/NXKit.axd/";
 
+        /// <summary>
+        /// PreApplicationStartMethod
+        /// </summary>
         public static void Start()
         {
             HttpApplication.RegisterModule(typeof(ViewModuleHttpModule));
@@ -22,34 +25,18 @@ namespace NXKit.View.Web.UI
         
         public void Init(HttpApplication context)
         {
-            context.BeginRequest += context_BeginRequest;
+            context.PostAuthenticateRequest += context_PostAuthenticateRequest;
         }
 
-        void context_BeginRequest(object sender, EventArgs args)
+        void context_PostAuthenticateRequest(object sender, EventArgs args)
         {
             var context = (HttpApplication)sender;
             if (context == null)
                 return;
 
+            // send to proper handler if under prefix
             if (context.Request.AppRelativeCurrentExecutionFilePath.StartsWith(PREFIX))
-            {
-                var name = context.Request.QueryString["m"];
-                var file = CompositionUtil.CreateContainer(CompositionUtil.DefaultGlobalCatalog)
-                    .GetExportedValues<IViewModuleResolver>()
-                    .Select(i => i.Resolve(name))
-                    .FirstOrDefault(i => i != null);
-                if (file != null)
-                {
-                    context.Response.StatusCode = 200;
-                    file(context.Response);
-                    context.Context.Response.End();
-                }
-                else
-                {
-                    context.Response.StatusCode = 404;
-                    context.Context.Response.End();
-                }
-            }
+                context.Context.RemapHandler(new ViewModuleHttpHandler());
         }
 
         public void Dispose()
