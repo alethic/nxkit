dbda9e57061cd50b522153db7f5da207578344b8
Jerome Haltom <jhaltom@isillc.com>
2014-10-06 14:36:20 -0500

    Fixed up nuspec and pack script.

--- nuget-pack.ps1
+++ nuget-pack.ps1
@@ -1,1 +1,12 @@
-Get-Item *\*.nuspec | %{ & .\.nuget\NuGet.exe pack ( $_.FullName -replace '\.nuspec$','.csproj' ) -Version 1.2.3.4 }
+$NuGetVersion=((packages\GitVersion.CommandLine.1.2.1\Tools\GitVersion.exe /output json) -join ' ' | ConvertFrom-Json).NuGetVersionV2
+$Properties="Configuration=Release;VisualStudioVersion=12.0"
+$OutputDirectory="bin\"
+
+if (!(Test-Path $OutputDirectory)) {
+    New-Item -ItemType Directory -Path $OutputDirectory
+}
+
+foreach ($i in get-item *\*.nuspec) {
+    $ProjectFile=( $i.FullName -replace '\.nuspec$','.csproj' )
+    .\.nuget\NuGet.exe pack $ProjectFile -Build -Symbols -Version $NuGetVersion -Properties $Properties -OutputDirectory $OutputDirectory
+}
