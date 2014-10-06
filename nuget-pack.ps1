6407c05efdf1d9e56120678824eef4df054e4649
Jerome Haltom <jhaltom@isillc.com>
2014-10-06 14:10:11 -0500

    Remove/all GitVersionTask

--- nuget-pack.ps1
+++ nuget-pack.ps1
@@ -1,1 +1,1 @@
-Get-Item *\*.nuspec | %{ & .\.nuget\NuGet.exe pack ( $_.FullName -replace '\.nuspec$','.csproj' ) }
+Get-Item *\*.nuspec | %{ & .\.nuget\NuGet.exe pack ( $_.FullName -replace '\.nuspec$','.csproj' ) -Version 1.2.3.4 }
