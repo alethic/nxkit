40099c54a1e8b774763e2184b0d95e48a5f1f8b9
Jerome Haltom <jhaltom@isillc.com>
2015-02-07 00:42:18 -0600

    Fixed up NuGet packing.

--- nuget-pack.ps1
+++ nuget-pack.ps1
@@ -3,7 +3,7 @@ $NuGetExe="$NuGetDirectory\NuGet.exe"
 
 & $NuGetExe restore "$NuGetDirectory\packages.config" -PackagesDirectory ".\packages"
 
-$NuGetVersion=((packages\GitVersion.CommandLine.1.2.1\Tools\GitVersion.exe /output json) -join ' ' | ConvertFrom-Json).NuGetVersionV2
+$NuGetVersion=((packages\GitVersion.CommandLine.2.0.1\Tools\GitVersion.exe /output json) -join ' ' | ConvertFrom-Json).NuGetVersionV2
 $Properties="Configuration=Release;VisualStudioVersion=12.0;CodeContractsRunCodeAnalysis=false;NuGetVersion=$NuGetVersion"
 $OutputDirectory="bin\"
 
