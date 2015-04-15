9be1a9028f82b6decdfb008468160f145c1f7c2f
Jerome Haltom <wasabi@cogito.cx>
2015-04-15 09:29:46 -0500

    Using a

---
+++ NXKit.View.Windows/ElementPresenter.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Windows;
+using System.Windows.Controls;
+using System.Xml.Linq;
+
+namespace NXKit.View.Windows
+{
+
+    public class ElementPresenter :
+        Control
+    {
+
+        /// <summary>
+        /// Initializes the static instance.
+        /// </summary>
+        static ElementPresenter()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ElementPresenter), new FrameworkPropertyMetadata(typeof(ElementPresenter)));
+        }
+
+        /// <summary>
+        /// Registers a dependency property as backing store for the Element property.
+        /// </summary>
+        public static readonly DependencyProperty ElementProperty = DependencyProperty.Register(
+            "Element",
+            typeof(XElement),
+            typeof(ElementPresenter),
+            new FrameworkPropertyMetadata(Element_PropertyChanged));
+
+        /// <summary>
+        /// Gets or sets the <see cref="XElement"/> to be presented.
+        /// </summary>
+        public XElement Element
+        {
+            get { return (XElement)GetValue(ElementProperty); }
+            set { SetValue(ElementProperty, value); }
+        }
+
+        /// <summary>
+        /// Invoked when the value of Element is changed.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        static void Element_PropertyChanged(DependencyObject sender, DependencyPropertyChangedEventArgs args)
+        {
+            Contract.Requires<ArgumentNullException>(sender != null);
+            Contract.Requires<ArgumentNullException>(args != null);
+
+            var ctrl = (ElementPresenter)sender;
+            var element = ctrl.Element;
+            if (element != null)
+            {
+                ctrl.SetValue(ElementModelItemPropertyKey, new ElementModelItem(element));
+                ctrl.SetValue(ElementTemplateResourceKeyPropertyKey, new ElementTemplateResourceKey(element.Name));
+            }
+            else
+            {
+                ctrl.SetValue(ElementModelItemPropertyKey, null);
+                ctrl.SetValue(ElementTemplateResourceKeyPropertyKey, null);
+            }
+        }
+
+        static readonly DependencyPropertyKey ElementModelItemPropertyKey = DependencyProperty.RegisterReadOnly(
+            "ElementModelItem",
+            typeof(ElementModelItem),
+            typeof(ElementPresenter),
+            new FrameworkPropertyMetadata());
+
+        public static readonly DependencyProperty ElementModelItemProperty = ElementModelItemPropertyKey.DependencyProperty;
+
+        /// <summary>
+        /// Gets the <see cref="ElementModelItem"/> that will be set as the DataContext of the template.
+        /// </summary>
+        public ElementModelItem ElementModelItem
+        {
+            get { return (ElementModelItem)GetValue(ElementModelItemProperty); }
+        }
+
+        static readonly DependencyPropertyKey ElementTemplateResourceKeyPropertyKey = DependencyProperty.RegisterReadOnly(
+            "ElementTemplateResourceKey",
+            typeof(ElementTemplateResourceKey),
+            typeof(ElementPresenter),
+            new FrameworkPropertyMetadata());
+
+        public static readonly DependencyProperty ElementTemplateResourceKeyProperty = ElementTemplateResourceKeyPropertyKey.DependencyProperty;
+
+        /// <summary>
+        /// Gets the <see cref="ElementTemplateResourceKey"/> that can be used to resolve the presented <see cref="XElement"/>'s template.
+        /// </summary>
+        public ElementTemplateResourceKey ElementTemplateResourceKey
+        {
+            get { return (ElementTemplateResourceKey)GetValue(ElementTemplateResourceKeyProperty); }
+        }
+
+    }
+
+}
---
+++ NXKit.View.Windows/ElementTemplateResourceKey.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Windows.Markup;
+using System.Xml.Linq;
+
+namespace NXKit.View.Windows
+{
+
+    public class ElementTemplateResourceKey
+    {
+
+        XName name;
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        public ElementTemplateResourceKey()
+            : base()
+        {
+
+        }
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="name"></param>
+        public ElementTemplateResourceKey(XName name)
+            : this()
+        {
+            Contract.Requires<ArgumentNullException>(name != null);
+
+            this.name = name;
+        }
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="expandedName"></param>
+        public ElementTemplateResourceKey(string expandedName)
+            : this(XName.Get(expandedName))
+        {
+            Contract.Requires<ArgumentNullException>(expandedName != null);
+            Contract.Requires<ArgumentException>(!string.IsNullOrWhiteSpace(expandedName));
+        }
+
+        /// <summary>
+        /// Gets or sets the <see cref="XName"/> referenced by this key.
+        /// </summary>
+        public XName Name
+        {
+            get { return name; }
+            set { Contract.Requires<ArgumentNullException>(value != null); name = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the expanded name referenced by this key.
+        /// </summary>
+        public string ExpandedName
+        {
+            get { return name.ToString(); }
+            set { Contract.Requires<ArgumentNullException>(value != null); name = XName.Get(value); }
+        }
+
+    }
+
+}
