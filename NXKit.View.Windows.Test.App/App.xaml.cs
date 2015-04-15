9d3491f80e1be3e1e4e1d0fafdbcb6f2cb01816b
Jerome Haltom <wasabi@cogito.cx>
2015-04-14 23:33:40 -0500

    Added beginnings of WPF support.

---
+++ NXKit.View.Windows.Test.App/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace NXKit.View.Windows.Test.App
+{
+
+    public partial class App : 
+        Application
+    {
+
+
+
+    }
+
+}
---
+++ NXKit.View.Windows.Test.App/DelegateCommand.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace NXKit.View.Windows.Test.App
+{
+
+    public class DelegateCommand :
+        ICommand
+    {
+
+        readonly Predicate<object> canExecute;
+        readonly Action<object> execute;
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="execute"></param>
+        public DelegateCommand(Action<object> execute)
+            : this(execute, null)
+        {
+
+        }
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="execute"></param>
+        /// <param name="canExecute"></param>
+        public DelegateCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void OnCanExecuteChanged()
+        {
+            if (CanExecuteChanged != null)
+                CanExecuteChanged(this, EventArgs.Empty);
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute == null ? true : canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            execute(parameter);
+        }
+
+    }
+
+}
