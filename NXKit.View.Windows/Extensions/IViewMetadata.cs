c40e4f5b4b693abe402a13096c1ffc89d2937cfc
Jerome Haltom <wasabi@cogito.cx>
2015-04-18 10:41:47 -0500

    Added a metadata extension and interface for abstracting some high level view properties out of the tree into the UI layer.

---
+++ NXKit.View.Windows/Extensions/IViewMetadata.cs
@@ -0,0 +1,17 @@
+﻿namespace NXKit.View.Windows.Extensions
+{
+
+    /// <summary>
+    /// Describes metadata available to the View layer.
+    /// </summary>
+    public interface IViewMetadata
+    {
+
+        /// <summary>
+        /// Gets whether or not the <see cref="XElement"/> is a metadata element.
+        /// </summary>
+        public bool IsMetadata { get; }
+
+    }
+
+}
---
+++ NXKit.XForms.View.Windows/Extensions/MetadataElements.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Diagnostics.Contracts;
+using System.Xml.Linq;
+
+using NXKit.Composition;
+using NXKit.View.Windows.Extensions;
+
+namespace NXKit.XForms.View.Windows.Extensions
+{
+
+    [Extension(typeof(IViewMetadata), "{http://www.w3.org/2002/xforms}label")]
+    [Extension(typeof(IViewMetadata), "{http://www.w3.org/2002/xforms}help")]
+    [Extension(typeof(IViewMetadata), "{http://www.w3.org/2002/xforms}hint")]
+    [Extension(typeof(IViewMetadata), "{http://www.w3.org/2002/xforms}alert")]
+    [PartMetadata(ScopeCatalog.ScopeMetadataKey, Scope.Object)]
+    public class MetadataElements :
+        ElementExtension,
+        IViewMetadata
+    {
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="element"></param>
+        [ImportingConstructor]
+        public MetadataElements(XElement element)
+            : base(element)
+        {
+            Contract.Requires<ArgumentNullException>(element != null);
+        }
+
+        public bool IsMetadata
+        {
+            get { return true; }
+        }
+
+    }
+
+}
