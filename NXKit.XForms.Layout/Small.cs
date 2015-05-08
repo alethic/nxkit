2fdca606248f69b092035118526e2c5f1d5e1c84
Jerome Haltom <jhaltom@isillc.com>
2015-05-08 17:09:33 -0500

    Added Small. Some aship form changes.

---
+++ NXKit.XForms.Layout/Small.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Diagnostics.Contracts;
+using System.Xml.Linq;
+
+using NXKit.Composition;
+
+namespace NXKit.XForms.Layout
+{
+
+    [Extension("{http://schemas.nxkit.org/2014/xforms-layout}small")]
+    [PartMetadata(ScopeCatalog.ScopeMetadataKey, Scope.Object)]
+    public class Small :
+        ElementExtension
+    {
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="element"></param>
+        [ImportingConstructor]
+        public Small(XElement element)
+            : base(element)
+        {
+            Contract.Requires<ArgumentNullException>(element != null);
+        }
+
+    }
+
+}
